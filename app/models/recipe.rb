b23a4f136a380abadd6ff790b0efe93703422020
Javivy <javierralejandroortga@gmail.com>
2023-10-24 16:20:47 -0400

    Add validations in recipe.rb model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :preparation_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :cooking_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :description, presence: true
+  validates :public, inclusion: { in: [true, false] }
+  validates :user_id, presence: true
 end
