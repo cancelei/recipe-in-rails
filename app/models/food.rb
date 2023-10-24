912311aa59c61bd54b4eea9686e423e2871863a1
cancelei <glauber.bannwart@gmail.com>
2023-10-24 12:22:55 -0300

    Update food and user models for food items

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,8 @@
 class Food < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, uniqueness: { scope: :user_id }
+  validates :measurement_unit, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
