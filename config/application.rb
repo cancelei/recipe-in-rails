3977cc8b693662242ef7c58a4675ce57ce11f986
cancelei <glauber.bannwart@gmail.com>
2024-02-08 16:21:53 -0300

    Fix CSS compression issue

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module RecipeInRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.assets.paths << Rails.root.join("app", "assets", "stylesheets")
-
+    config.assets.css_compressor = nil
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
